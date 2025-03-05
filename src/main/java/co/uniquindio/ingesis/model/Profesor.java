3e60ba394babc4e5139507119579ec6bd6e3453c
BrahianDavidGarcia <brahiand320519@gmail.com>
2025-03-05 06:46:14 -0500

    Merge profesor y estudiante

---
+++ src/main/java/co/uniquindio/ingesis/model/Profesor.java
@@ -0,0 +1,34 @@
+package co.uniquindio.ingesis.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data @AllArgsConstructor @NoArgsConstructor
+@Table(name = "profesor")
+public class Profesor implements Serializable {
+
+    //_____________FIELDS__________________//
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_profesor")
+    private Integer id;
+
+    @Column(name = "documento_profesor", nullable = false, updatable = false, unique = true)
+    private String documento;
+
+    @Column(name = "nombre_profesor", nullable = false, updatable = false)
+    private String nombre;
+
+    @Column(name = "email_profesor", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password_profesor", nullable = false)
+    private String password;
+
+}
+
